c1a515651e63709afdb6982bc799462977b1fa69
wilmort <contributor4@proton.me>
2024-04-06 10:51:15 -0400

    adding dirs to seperate apple silicon and linux implementations

---
+++ apple/hello_silicon/readme.md
@@ -0,0 +1,7 @@
+### HelloSilicon
+
+"An introduction to ARM64 assembly on Apple Silicon Macs"
+
+https://github.com/below/HelloSilicon
+
+
--- lw_tut/001/hello.s
+++
@@ -1,7 +0,0 @@
-.global _start
-.section .text
-
-_start:
-	mov x0, #42		// x0 gets 42.
-	mov x8, #0x5d		// x8 gets syscall exit.
-	svc 0			
